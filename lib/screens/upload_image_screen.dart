4a9b04668faeaf0106a85c8d0b1ee5e8e86dc0c9
saboor-flutter <saboor.flutter@tafsol.com>
2025-10-21 18:15:57 +0500

    feat: implement availability management with UI enhancements and view model integration

--- lib/screens/upload_image_screen.dart
+++ lib/screens/upload_image_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'package:beauty_near_sp/screens/bot_nav_bar_page.dart';
 import 'package:beauty_near_sp/view_models/auth_view_model.dart';
+import 'package:beauty_near_sp/widgets/dialog%20box/success_dialog_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -49,7 +51,9 @@ class UploadImageScreen extends StatelessWidget {
 
             GestureDetector(
               onTap: () async {
-                await context.read<AuthViewModel>().setProfileImagePath(context: context);
+                await context.read<AuthViewModel>().setProfileImagePath(
+                  context: context,
+                );
               },
               child: Container(
                 width: double.infinity,
@@ -67,9 +71,15 @@ class UploadImageScreen extends StatelessWidget {
                               width: 200.w,
                               height: 200.h,
                               decoration: BoxDecoration(
-                              //  borderRadius: BorderRadius.circular(12.r),
+                                //  borderRadius: BorderRadius.circular(12.r),
                                 image: DecorationImage(
-                                  image: FileImage(File(context.watch<AuthViewModel>().getProfileImagePath!)),
+                                  image: FileImage(
+                                    File(
+                                      context
+                                          .watch<AuthViewModel>()
+                                          .getProfileImagePath!,
+                                    ),
+                                  ),
                                   fit: BoxFit.cover,
                                 ),
                               ),
@@ -90,7 +100,11 @@ class UploadImageScreen extends StatelessWidget {
                     : Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SvgPicture.asset(SvgAssets.upload, height: 35.h, width: 35.w),
+                          SvgPicture.asset(
+                            SvgAssets.upload,
+                            height: 35.h,
+                            width: 35.w,
+                          ),
                           SizedBox(height: 15.h),
                           Text(
                             'Upload Image',
@@ -129,7 +143,17 @@ class UploadImageScreen extends StatelessWidget {
             Expanded(
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.pushNamed(context, availabilityScreen);
+                  showSuccessDialog(
+                    screenContext: context,
+                    desc: 'Your Account Successfully\nCreated',
+                    onSuccess: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        BotNavPage.routeName,
+                        (route) => false,
+                      );
+                    },
+                  );
                 },
                 child: Text('Next'),
               ),
@@ -137,7 +161,17 @@ class UploadImageScreen extends StatelessWidget {
             SizedBox(width: 10.w),
             GestureDetector(
               onTap: () {
-                Navigator.pushNamed(context, availabilityScreen);
+                showSuccessDialog(
+                  screenContext: context,
+                  desc: 'Your Account Successfully\nCreated',
+                  onSuccess: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      BotNavPage.routeName,
+                      (route) => false,
+                    );
+                  },
+                );
               },
               child: Container(
                 padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 22.w),
--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -9,4 +9,18 @@ enum PaymentType {
   const PaymentType(this.label);
 }
 
+enum Days {
+  monday('Monday'),
+  tuesday('Tuesday'),
+  wednesday('Wednesday'),
+  thursday('Thursday'),
+  friday('Friday'),
+  saturday('Saturday'),
+  sunday('Sunday');
+
+  final String label;
+
+  const Days(this.label);
+}
+
 
